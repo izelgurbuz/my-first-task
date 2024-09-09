90dd8d74b11ff358fc435ec5c3bbdd7a07f5d197
Izel Gurbuz <izelgurbuz@Izels-MacBook-Air.local>
2024-09-09 14:39:07 +0100

    Login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { login } from "../api/authApi";
 
-const Login = () => {
-  return <div>Login </div>;
+const Login: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [loading, setLoading] = useState(false); // State to track loading
+
+  const { mutateAsync: loginUser } = useMutation({
+    mutationFn: login,
+    onSuccess: () => {},
+  });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    setLoading(true);
+    try {
+      await loginUser({ email, password });
+      alert("User logged in successfully!");
+    } catch (error) {
+      console.error("Error during registration:", error);
+      alert("Login failed!");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+        required
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        required
+      />
+      <button type="submit" disabled={loading}>
+        {loading ? "Logging in..." : "Login"}
+      </button>
+    </form>
+  );
 };
 
 export default Login;
